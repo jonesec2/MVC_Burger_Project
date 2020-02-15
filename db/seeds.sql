b91984776453fd7cbae6aa236112d08c570bb8a1
pb-totoro <jonesec2@gmail.comgit config --global user.email jonesec2@gmail.com>
2020-02-15 13:30:52 -0500

    Adding heroku and JAWSDB to connection

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
 Insert Into burgers (burger_name, devoured) Values ("Double quarter pounder", 0);
 Insert Into burgers (burger_name, devoured) Values ("Big Mac", 0);
 Insert Into burgers (burger_name, devoured) Values ("Dave's Double with cheese", 1);
-
-'= 1where burger_id = id = 2'
