4ff8693807b0158e3d9143669178ea17b8535405
pb-totoro <jonesec2@gmail.comgit config --global user.email jonesec2@gmail.com>
2020-02-15 13:58:12 -0500

    Resized text size, and changed the row sizes

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
       host: "localhost",
       port: 3306,
       user: "root",
-      password: "Testing_1",
+      password: "Freaky1zepp!",
       database: "burgers_db"
    });
 }
